8e1630094100114af4a9de1f09cb07655bad056b
Jakub Antonín Štigler <jakubstig@gmail.com>
2024-03-06 12:56:37 +0100

    Create Environment interface

--- ija/ija2023/homework1/common/Environment.java
+++ ija/ija2023/homework1/common/Environment.java
@@ -1,3 +1,13 @@
 public interface Environment {
-    
+    boolean addRobot(Robot robot);
+
+    boolean containsPosition(Position pos);
+
+    boolean createObstacleAt(int row, int col);
+
+    boolean obstacleAt(int row, int col);
+
+    boolean obstacleAt(Position p);
+
+    boolean robotAt(Position p);
 }
